62b0ef6bf68efc3eaa48f327e532d17811919710
Johan Bergström <bergsjoh@gmail.com>
2016-06-09 09:15:35 +0200

    Fixing comments from @evgeni

--- bootstrap.py
+++ bootstrap.py
@@ -82,7 +82,7 @@ parser.add_option("--rhn-migrate-args", dest="rhsmargs", default="", help="Which
 parser.add_option("-u", "--update", dest="update", action="store_true", help="Fully Updates the System")
 parser.add_option("-v", "--verbose", dest="verbose", action="store_true", help="Verbose output")
 parser.add_option("-f", "--force", dest="force", action="store_true", help="Force registration (will erase old katello and puppet certs)")
-parser.add_option("-d", "--add-domain", dest="add_domain", action="store_true", help="Automatically add the clients domain to Satellite6")
+parser.add_option("--add-domain", dest="add_domain", action="store_true", help="Automatically add the clients domain to Foreman")
 parser.add_option("--remove", dest="remove", action="store_true", help="Instead of registring the machine to Foreman remove it")
 parser.add_option("-r", "--release", dest="release", default=RELEASE, help="Specify release version")
 parser.add_option("-R", "--remove-rhn-packages", dest="removepkgs", action="store_true", help="Remove old Red Hat Network Packages")
@@ -377,9 +377,6 @@ def create_domain(domain, orgid, locid):
         jsondata = json.loads('{"domain": {"name": "%s", "organization_ids": [%s], "location_ids": [%s]}}' % (domain, orgid, locid))
         print_running("Calling Foreman API to create domain %s associated with the org & location" % domain)
         post_json(myurl, jsondata)
-        return domain
-    else:
-        return domain
 
 def create_host():
     myhgid = return_matching_foreman_key('hostgroups', 'title="%s"' % options.hostgroup, 'id', False)
@@ -388,9 +385,9 @@ def create_host():
     if DOMAIN:
         if options.add_domain:
             create_domain(DOMAIN, myorgid, mylocid)
-        try:
-            mydomainid = return_matching_foreman_key('domains', 'name="%s"' % DOMAIN, 'id', False)
-        except:
+
+        mydomainid = return_matching_foreman_key('domains', 'name="%s"' % DOMAIN, 'id', True)
+        if not mydomainid: 
             print_generic("Domain %s doesn't exist in Foreman, consider using the --add-domain option." % DOMAIN)
             sys.exit(2)
     else:
