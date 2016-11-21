1f98abbe4c28d4796a11e823e7a1f01fb36ce390
Eric D Helms <ericdhelms@gmail.com>
2016-11-21 11:44:21 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Katello Client Bootstrap',
+      version='1.2.0',
+      description='Bootstrap Script for migrating systems to Foreman & Katello',
+      author='Rich Jerrido',
+      author_email='rjerrido@outsidaz.org',
+      url='https://github.com/Katello/katello-client-bootstrap',
+      packages=['bootstrap'],
+     )
