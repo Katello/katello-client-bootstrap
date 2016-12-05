2cb9987eff4eb48a26a639d0a6d3261f778d25ca
Rich Jerrido <rjerrido@outsidaz.org>
2016-12-05 10:07:37 -0500

    update setup.py to 1.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='katello-client-bootstrap',
-      version='1.2.0',
+      version='1.2.2',
       description='Bootstrap Script for migrating systems to Foreman & Katello',
       author='Rich Jerrido',
       author_email='rjerrido@outsidaz.org',
