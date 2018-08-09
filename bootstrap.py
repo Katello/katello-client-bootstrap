44d7845832e3a8303ff0c011357f044cd47696b3
Samuel MARTIN MORO <faust64@gmail.com>
2018-08-09 10:18:06 +0200

    docs(puppet.conf): bunch of links advocating for avoiding caps in certname

--- bootstrap.py
+++ bootstrap.py
@@ -473,6 +473,15 @@ ssldir = /etc/puppetlabs/puppet/ssl
         main_section += "digest_algorithm = sha256"
         print_generic("System is in FIPS mode. Setting digest_algorithm to SHA256 in puppet.conf")
     puppet_conf = open(puppet_conf_file, 'wb')
+
+    # set puppet.conf certname to lowercase FQDN, as capitalized characters would
+    # get translated anyway generating our certificate
+    # * https://puppet.com/docs/puppet/3.8/configuration.html#certname
+    # * https://puppet.com/docs/puppet/4.10/configuration.html#certname
+    # * https://puppet.com/docs/puppet/5.5/configuration.html#certname
+    # other links mentioning capitalized characters related issues:
+    # * https://grokbase.com/t/gg/puppet-users/152s27374y/forcing-a-variable-to-be-lower-case
+    # * https://groups.google.com/forum/#!topic/puppet-users/vRAu092ppzs
     puppet_conf.write("""
 %s
 [agent]
