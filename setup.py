a1d524e7b86cee2fc55f2cbe00e7a35ec21fd313
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-09-10 18:48:15 +0200

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -14,4 +14,16 @@ setup(
     license='GPL-2',
     url='https://github.com/Katello/katello-client-bootstrap',
     scripts=['bootstrap.py'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Systems Administration',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
 )
