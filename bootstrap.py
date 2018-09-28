6319295982482fa1b79ea23b3f2d4ba2f30de085
Evgeni Golov <evgeni@golov.de>
2018-09-28 18:45:18 +0200

    don't write curl config as binary (#270)

--- bootstrap.py
+++ bootstrap.py
@@ -286,7 +286,7 @@ def get_bootstrap_rpm(clean=False, unreg=True):
     if options.download_method == "https":
         print_generic("Writing custom cURL configuration to allow download via HTTPS without certificate verification")
         curl_config_dir = tempfile.mkdtemp()
-        curl_config = open(os.path.join(curl_config_dir, '.curlrc'), 'wb')
+        curl_config = open(os.path.join(curl_config_dir, '.curlrc'), 'w')
         curl_config.write("insecure")
         curl_config.close()
         os.environ["CURL_HOME"] = curl_config_dir
