811daf6e9b7b6986a5a4757af9c1d6b02111b17f
Evgeni Golov <evgeni@golov.de>
2022-03-17 09:34:27 +0100

    release 1.7.8

--- bootstrap.py
+++ bootstrap.py
@@ -63,7 +63,7 @@ except ImportError:
 import rpm  # pylint:disable=import-error
 
 
-VERSION = '1.7.7'
+VERSION = '1.7.8'
 
 # Python 2.4 only supports octal numbers by prefixing '0'
 # Python 3 only support octal numbers by prefixing '0o'
